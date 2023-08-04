eda097f6f9cb5863b44bf10d7961878c61fad513
ClemCa <perso.catorc@gmail.com>
2023-08-04 14:33:09 +0200

    Fixes to window & tests

--- Tests/MainTests.cs
+++ Tests/MainTests.cs
@@ -58,13 +58,13 @@ namespace Game
                         thread3.Start();
                         break;
                     default:
-                        throw new NotImplementedException("Test "+test+" does not exist");
+                        throw new NotImplementedException("Test " + test + " does not exist");
                 }
                 KeepAlive();
             }
             catch (Exception e)
             {
-                Console.WriteLine("An error occured: "+e.Message);
+                Console.WriteLine("An error occured: " + e.Message);
                 Console.WriteLine(e.Source);
                 Console.WriteLine(e.StackTrace);
             }
@@ -72,14 +72,14 @@ namespace Game
         public static void MainStandard()
         {
             var engineargs = new EngineArgs(384, 216, loadPath: "assets");
-            var window = new Window(engineargs, WindowFlags.NOVSYNC|WindowFlags.S1);
+            var window = new Window(engineargs, WindowFlags.NOVSYNC | WindowFlags.S1);
 
             foreground = new("Sonic_md_fg1.tmx");
 
-            Bitmap bitmap = new(256, 256, 8);
+            Bitmap bitmap = new(256, 256, 8)
             {
-                Palette palette = new(16);
-            }
+                Palette = new(16)
+            };
 
             window.Engine.Layers[0].Bitmap = bitmap;
             window.Engine.Layers[0].Bitmap.Palette.SetColor(1, 255, 0, 0);
@@ -88,19 +88,21 @@ namespace Game
             Console.WriteLine(window.Engine.LastError);
 
             foreground.SetTileAttribute(23, 0, flipy: true);
+
             window.BeforeFrame += BeforeFrame;
             window.AfterFrame += AfterFrame;
         }
         public static void MainDelayed()
         {
             var engineargs = new EngineArgs(384, 216, loadPath: "assets");
-            var window = new Window(engineargs, WindowFlags.NOVSYNC|WindowFlags.S1, autostart: false);
+            var window = new Window(engineargs, WindowFlags.NOVSYNC | WindowFlags.S1, autostart: false);
 
             foreground = new("Sonic_md_fg1.tmx");
-            Bitmap bitmap = new(256, 256, 8);
+
+            Bitmap bitmap = new(256, 256, 8)
             {
-                Palette palette = new(16);
-            }
+                Palette = new(16)
+            };
             window.Engine.Layers[0].Bitmap = bitmap;
             window.Engine.Layers[0].Bitmap.Palette.SetColor(1, 255, 0, 0);
             window.Engine.Layers[0].Bitmap = bitmap;
@@ -108,16 +110,15 @@ namespace Game
             Console.WriteLine(window.Engine.LastError);
 
             foreground.SetTileAttribute(23, 0, flipy: true);
-
+            window.Start();
             window.BeforeFrame += BeforeFrame;
             window.AfterFrame += AfterFrame;
 
-            window.Start();
         }
         public static void MainManaged() // engine is still started using the Window
         {
             var engine = new Engine(384, 216, loadPath: "assets");
-            var window = new Window(null, WindowFlags.NOVSYNC|WindowFlags.S1, autostart: false);
+            var window = new Window(null, WindowFlags.NOVSYNC | WindowFlags.S1, autostart: false);
 
             // not required, only if Window.Engine is used by later code
             window.SetManagedEngine(new EngineRef(() => engine, (e) => engine = e));
@@ -146,7 +147,7 @@ namespace Game
         public static void MainLegacy() // or fully managed
         {
             var engine = new Engine(384, 216, loadPath: "assets");
-            var window = new Window(null, WindowFlags.NOVSYNC|WindowFlags.S1, autostart: false);
+            var window = new Window(null, WindowFlags.NOVSYNC | WindowFlags.S1, autostart: false);
 
             // not required, only if Window.Engine is used by later code
             window.SetManagedEngine(new EngineRef(() => engine, (e) => engine = e));
@@ -167,7 +168,7 @@ namespace Game
             foreground.SetTile(23, 0, tile);
             Console.WriteLine(engine.LastError);
 
-            while(window.Process)
+            while (window.Process)
             {
                 BeforeFrame(window, new());
                 window.DrawFrame(0);
@@ -178,6 +179,7 @@ namespace Game
 
         private static void BeforeFrame(Window window, FrameArgs args)
         {
+            Console.WriteLine("BeforeFrame");
             return;
             window.Engine.Layers[0].Bitmap.Clear();
             window.Engine.Layers[0].Bitmap.DrawRect(new Rect(20, 20, 16, 16), 1, false);
